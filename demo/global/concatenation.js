8bca11ac3a8b84837d11669b011d97d8c7573752
glodie <glodie.tshimini@gmail.com>
2024-10-08 12:42:27 +0200

    feat: demo concat with $

---
+++ demo/global/concatenation.js
@@ -0,0 +1,8 @@
+const hello = 'hello'
+const world = 'world'
+
+// concatenation old fashion
+const helloWorld = hello + ' ' + world
+
+// concaténation new fashion avec ` et $
+const helloWorld2 = `${hello} ${world}`
